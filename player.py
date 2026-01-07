9824ccc264ef536188207d693e3710975a6fb914
radiantlegends <radiantlegends@gmail.com>
2026-01-06 21:34:44 -0500

    added asteroids

--- player.py
+++ player.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import PLAYER_RADIUS, LINE_WIDTH, PLAYER_TURN_SPEED, PLAYER_SPEED
+from constants import *
 from circleshape import CircleShape
 
 class Player(CircleShape):
